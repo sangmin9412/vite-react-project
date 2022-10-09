51afc547815c9fb941cd31e03594be37c534e878
sanmgin9412 <tkdals9048@naver.com>
2022-10-10 05:35:56 +0900

    장바구니 mock API 완료

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { Product } from "../../graphql/products";
+import { cartItemSelector } from "../../recoils/cart";
+import { useRecoilState } from "recoil";
 
 const ProductItem = ({
   id,
@@ -8,14 +10,22 @@ const ProductItem = ({
   title,
   description,
   createdAt,
-}: Product) => (
-  <li className="product-item">
-    <Link to={`/products/${id}`}>
-      <p className="product-item__title">{title}</p>
-      <img className="product-item__image" src={imageUrl} />
-      <p className="product-item__price">${price}</p>
-    </Link>
-  </li>
-);
+}: Product) => {
+  const [cartAmount, setCartAmount] = useRecoilState(cartItemSelector(id));
+  const addToCart = () => setCartAmount((cartAmount || 0) + 1);
+  return (
+    <li className="product-item">
+      <Link to={`/products/${id}`}>
+        <p className="product-item__title">{title}</p>
+        <img className="product-item__image" src={imageUrl} />
+        <p className="product-item__price">${price}</p>
+      </Link>
+      <button className="product-item__add-cart" onClick={addToCart}>
+        담기
+      </button>
+      <span>{cartAmount || 0}</span>
+    </li>
+  );
+};
 
 export default ProductItem;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import GlobalLayout from "./pages/_layout";
 import Index from "./pages/index";
 import CartIndex from "./pages/cart/index";
-// import PaymentIndex from './pages/payment/index'
+import PaymentIndex from "./pages/payment/index";
 import ProductsIndex from "./pages/products/index";
 import ProductsId from "./pages/products/[id]";
-// import AdminIndex from './pages/admin/index'
+import AdminIndex from "./pages/admin/index";
 
 export const routes = [
   {
@@ -13,10 +13,10 @@ export const routes = [
     children: [
       { path: "/", element: <Index />, index: true },
       { path: "/cart", element: <CartIndex />, index: true },
-      // { path: '/payment', element: <PaymentIndex />, index: true },
+      { path: "/payment", element: <PaymentIndex />, index: true },
       { path: "/products", element: <ProductsIndex />, index: true },
       { path: "/products/:id", element: <ProductsId /> },
-      // { path: '/admin', element: <AdminIndex />, index: true },
+      { path: "/admin", element: <AdminIndex />, index: true },
     ],
   },
 ];
